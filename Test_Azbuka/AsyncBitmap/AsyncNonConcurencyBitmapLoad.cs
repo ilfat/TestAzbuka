190e593831bfde64abb9944b8cf6742d1d93fafb
ilfat <ilfat1211@gmail.com>
2015-02-03 10:54:30 +0300

    +расширяющий метод для загрузки изображения

---
+++ Test_Azbuka/AsyncBitmap/AsyncNonConcurencyBitmapLoad.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Test_azbuka;
+using Android.Widget;
+using Android.Content.Res;
+
+namespace AsyncNonConcurencyBitmapLoad
+{
+	public static class AsyncNonConcurencyBitmapLoad
+	{
+		public static void FromUrlNonConcurency(this ImageView imageView, string url, Resources resources) {
+			BitmapWorkerTask.StartNew (imageView, url, resources);
+		}
+	}
+}
+
